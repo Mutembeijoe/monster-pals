7ec495179a91c986f330a5eacb14f28876d17a6a
mutembeijoe <mutembeijoe@gmail.com>
2020-04-04 00:58:41 +0300

    Updates

---
+++ precache-manifest.3a622f4912d3e49cc153715ed9e20ce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c8f9ef3289a8e70a34f5ce48cb9370",
+    "url": "/monster-pals/index.html"
+  },
+  {
+    "revision": "f4ee34ccf8aa5efb206e",
+    "url": "/monster-pals/static/css/main.f846c69e.chunk.css"
+  },
+  {
+    "revision": "832a7c345b29d869587a",
+    "url": "/monster-pals/static/js/2.575cd690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-pals/static/js/2.575cd690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ee34ccf8aa5efb206e",
+    "url": "/monster-pals/static/js/main.4d7c39a7.chunk.js"
+  },
+  {
+    "revision": "868f2ec5a149f8746001",
+    "url": "/monster-pals/static/js/runtime-main.6333c6cd.js"
+  }
+]);
